f1dbd65a1af7916b148c722a140e2d36f4ebbeee
Gustavo Di Martino Paixao <136792335+GustavoDMPaixao@users.noreply.github.com>
2024-12-10 12:15:11 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+const boardRegions = document.querySelectorAll("#gameBoard span");
+let vBoard = [];
+let turnPlayer = "";
+
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer);
+  document.getElementById("turnPlayer").innerText = playerInput.value;
+}
+
+function initializeGame() {
+  vBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  turnPlayer = "player1";
+  // Update title if necessary
+  document.querySelector("h2").innerHTML =
+    'Vez de: <span id="turnPlayer"></span>';
+  updateTitle();
+  // Clear board and add eventlistener
+  boardRegions.forEach(function (element) {
+    element.classList.remove("win");
+    element.innerText = "";
+    element.classList.add("cursor-pointer");
+    element.addEventListener("click", handleBoardClick);
+  });
+}
+// Verify if 3 equal regions and return them
+function getWinRegions() {
+  const winRegions = [];
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[0][1] &&
+    vBoard[0][0] === vBoard[0][2]
+  )
+    winRegions.push("0.0", "0.1", "0.2");
+  if (
+    vBoard[1][0] &&
+    vBoard[1][0] === vBoard[1][1] &&
+    vBoard[1][0] === vBoard[1][2]
+  )
+    winRegions.push("1.0", "1.1", "1.2");
+  if (
+    vBoard[2][0] &&
+    vBoard[2][0] === vBoard[2][1] &&
+    vBoard[2][0] === vBoard[2][2]
+  )
+    winRegions.push("2.0", "2.1", "2.2");
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][0] &&
+    vBoard[0][0] === vBoard[2][0]
+  )
+    winRegions.push("0.0", "1.0", "2.0");
+  if (
+    vBoard[0][1] &&
+    vBoard[0][1] === vBoard[1][1] &&
+    vBoard[0][1] === vBoard[2][1]
+  )
+    winRegions.push("0.1", "1.1", "2.1");
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][2] &&
+    vBoard[0][2] === vBoard[2][2]
+  )
+    winRegions.push("0.2", "1.2", "2.2");
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][1] &&
+    vBoard[0][0] === vBoard[2][2]
+  )
+    winRegions.push("0.0", "1.1", "2.2");
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][1] &&
+    vBoard[0][2] === vBoard[2][0]
+  )
+    winRegions.push("0.2", "1.1", "2.0");
+  return winRegions;
+}
+// Disable an already played square
+function disableRegion(element) {
+  element.classList.remove("cursor-pointer");
+  element.removeEventListener("click", handleBoardClick);
+}
+// Paint the winning row/column
+function handleWin(regions) {
+  regions.forEach(function (region) {
+    document
+      .querySelector('[data-region="' + region + '"]')
+      .classList.add("win");
+  });
+  const playerName = document.getElementById(turnPlayer).value;
+  document.querySelector("h2").innerHTML = playerName + " venceu!";
+}
+
+function handleBoardClick(ev) {
+  const span = ev.currentTarget;
+  const region = span.dataset.region; // N.N
+  const rowColumnPair = region.split("."); // ["N", "N"]
+  const row = rowColumnPair[0];
+  const column = rowColumnPair[1];
+  // Tick the square played with X or O
+  if (turnPlayer === "player1") {
+    span.innerText = "X";
+    vBoard[row][column] = "X";
+  } else {
+    span.innerText = "O";
+    vBoard[row][column] = "O";
+  }
+  // Clear console and show virtaul board
+  console.clear();
+  console.table(vBoard);
+  // Disable function
+  disableRegion(span);
+  // Verify if winners
+  const winRegions = getWinRegions();
+  if (winRegions.length > 0) {
+    handleWin(winRegions);
+  } else if (vBoard.flat().includes("")) {
+    turnPlayer = turnPlayer === "player1" ? "player2" : "player1";
+    updateTitle();
+  } else {
+    document.querySelector("h2").innerHTML = "Empate!";
+  }
+}
+// Add event to start button
+document.getElementById("start").addEventListener("click", initializeGame);
